662471bb6bbbc966810ff3fe36af47ccac783593
unknown <nonce@protonmail.ch>
2015-04-01 21:57:31 -0400

    Added implementation for BST class and tree structure

--- Header.h
+++ Header.h
@@ -1,6 +1,20 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+struct Node 
+{
+	int data;
+	Node* left;
+	Node* right;
+};
 
+class BST
+{
+public:
+	BST();
+	Node* GetNewNode(int data);
+	Node* Insert(Node* root, int data);
+	bool Search(Node* root, int data);
+};
 
 #endif 
