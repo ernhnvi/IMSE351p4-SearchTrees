04e9a9f3234d0b76c3ed3b36d64237ee041049a5
unknown <nonce@protonmail.ch>
2015-04-06 20:48:07 -0400

    Added invalid entry handling, display total comparisons for all data structures

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include "Data_Structure.h"
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <vector>
 #include <fstream>
 #include <sstream>
@@ -9,52 +10,72 @@
 
 using namespace std;
 
+#undef max		// windows include defines a macro max(), need to undefine it to use max in line 71 to ignore user entry if invalid
+
 string generate_numFile();
 string validate_userFile();
 void fillTree(Node* &root);
 void printTree(DataStructure &ds, Node* &root);
-void numEntry(DataStructure &ds, Node* &root);
+int numEntry(DataStructure &ds, Node* &root);
 
 int main()
 {
-	char entry;
-	string file;
+		int entry;
 
-	cout << endl;
-	cout << "                              Main Menu                            " << endl;
-	cout << "                                                                   " << endl;
-	cout << "   1. Automatically generate file containing 150 random integers.  " << endl;
-	cout << "   2. Import and use existing file containing 150 random integers. " << endl;
-	cout << "                                                                   " << endl;
-	cout << "   c. Close                                                        " << endl;
-	cout << endl;
-	cout << "   > ";
-	cin >> entry;
+		string file;
 
-	switch (entry)
-	{
-	case '1':
-		file = generate_numFile();
-		break;
-	case '2':
-		file = validate_userFile();
-		break;
-	case 'c':
-		break;
-	default:
-		cout << "Invalid entry." << endl;
-		break;
-	}
+		cout << endl;
+		cout << "                              Main Menu                            " << endl;
+		cout << "                                                                   " << endl;
+		cout << "   1. Automatically generate file containing 150 random integers.  " << endl;
+		cout << "   2. Import and use existing file containing 150 random integers. " << endl;
+		cout << "                                                                   " << endl;
+		cout << "   3. Close                                                        " << endl;
+		cout << endl;
+		cout << "   > ";
+		cin >> entry;
 
-	DataStructure ds;
-	Node* root = NULL;
+		if (cin.good())		// if user entry was an integer
+		{
+			switch (entry)
+			{
+			case 1:
+				file = generate_numFile();
+				break;
+			case 2:
+				file = validate_userFile();
+				break;
+			case 3:
+				break;
+			default:
+				cout << "\t****INVALID ENTRY****" << endl;
+				Sleep(500);
+				system("CLS");
+				main();
+				break;
+			}
+			if (entry != 3)
+			{
+				DataStructure ds;
+				Node* root = NULL;
 
-	ds.unsorted_fill(file);
-	ds.fillTree(root);
-	printTree(ds, root);
-	numEntry(ds, root);
-
-	return 0;
+				ds.unsorted_fill(file);
+				ds.fillTree(root);
+				printTree(ds, root);
+				numEntry(ds, root);
+			}
+		}
+		else
+		{
+			cout << "\t****INVALID ENTRY****" << endl;
+			Sleep(500);
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');	// ignore to the end of line for user entry
+			system("CLS");
+			main();
+		}
+		
+		return 0;
 }
 
 string generate_numFile()
@@ -64,7 +85,7 @@ string generate_numFile()
 
 	std::random_device rd;
 
-	for (unsigned int x = 0; x < 150; x++)
+	for (unsigned int x = 0; x < 150; x++)	// fill fill with 150 integers
 	{
 		int num = (rd() % 1000) - 500;	// generate random number between +500 and -500
 		generatedFile << num << " ";
@@ -77,11 +98,19 @@ string generate_numFile()
 string validate_userFile()
 {
 	string fileName;
-	cout << "Enter name of file: ";
+	cout << "   Enter name of file: ";
 	cin >> fileName;
 
 	vector<string> ext = { ".txt", ".dat" };
 	vector<string> matchingFiles;
+
+	if (fileName.length() <= 4)
+	{
+		cout << "\t****INVALID FILE NAME****" << endl;
+		Sleep(500);
+		system("CLS");
+		main();
+	}
 
 	ifstream dataFile(fileName, ios::in);
 
@@ -107,27 +136,31 @@ string validate_userFile()
 
 		if (matchingFiles.empty())	// if userFile did not contain a matching substring to any index of ext and appending each extension in ext to the end of userFile did not result in a match to an existing file 
 		{
-			cout << "Unable to locate file." << endl;
-			Sleep(350);
+			cout << "\t****UNABLE TO LOCATE FILE****" << endl;
+			Sleep(500);
 			system("CLS");
 			main();
 		}
 
 		else if (matchingFiles.size() > 0)
 		{
-			cout << matchingFiles.size() << " potential file(s) detected. Enter the number cooresponding to the correct filename or 'm' to return to the menu." << endl;
+			system("CLS");
+			cout << matchingFiles.size() << " potential file(s) detected.\n\tEnter the number cooresponding to the correct filename" << endl;
+			cout << "\tOR enter 'm' to select a different option from the menu." << endl;
 
 			int count = 0;
+			cout << "\nFile(s) Detected" << endl;
 			for (unsigned int x = 0; x < matchingFiles.size(); x++)
 			{
 				count++;
-				cout << "(" << count << ") " << matchingFiles[x] << endl;
+				cout << "\t(" << count << ") " << matchingFiles[x] << endl;
 			}
 			cout << endl;
 			cin >> response;
 
 			if (response == 'm' || response == 'M')
 			{
+				system("CLS");
 				main();
 			}
 			else if ((int(response) - 48) > count)
@@ -209,109 +242,113 @@ void printTree(DataStructure &ds, Node* &root)
 		}
 		cout << std::endl;
 	}
-	system("PAUSE");
+	//system("PAUSE");
 }
 
-void numEntry(DataStructure &ds, Node* &root)
+int numEntry(DataStructure &ds, Node* &root)
 {
 	int inCounter = 5;
 	int outCounter = 5;
 	int numEntry;
+
 	vector<int> inList;
 	vector<int> outList;
 
 	while (inList.size() + outList.size() != 10)
-	{
-		system("CLS");
-		cout << "Found In List\t";
-		for (unsigned int i = 0; i < inList.size(); i++)
-		{
-			cout << inList[i] << " ";
-		}
-		cout << endl;
-
-		cout << "Not In List\t";
-		for (unsigned int i = 0; i < outList.size(); i++)
-		{
-			cout << outList[i] << " ";
-		}
-		cout << "\n\n";
-		cout << "Enter " << 10 - inList.size() + outList.size() << " numbers" << endl;
-		cout << inCounter  << " that are from the number list " << endl;
-		cout << outCounter << " that are not in the number list" << endl;
+	{	
+		cout << "Enter " << 10 - (inList.size() + outList.size()) << " numbers" << endl;
+		cout << inCounter  << " more matching file data" << endl;
+		cout << outCounter << " more NOT matching file data" << endl;
 		cin >> numEntry;
+		
+		system("CLS");
 
 		if (!cin)
 		{
 			cout << "Invalid character entry." << endl;
+			system("PAUSE");
 			break;
 		}
 		else
 		{
 			bool inFull = false; bool outFull = false;
+
 			while (!inFull || !outFull)
 			{
+				// Determines if user entry exists in unsorted list, if 5 entries already exist for found/not found numbers
+				// Prints unsorted array comparison count for current entry and total comparisons for unsorted array using linear search 
 				if (ds.unsorted_search(numEntry))
 				{
 					if (inCounter == 0)
 					{
-						cout << "5 number entries that match numbers in list already exist." << endl;
+						cout << "You already entered 5 numbers matching file data." << endl;
 						inFull = true;
-						system("PAUSE");
-						ds.comparisonReset();
+						system("PAUSE"); system("CLS");
 						break;
 					}
 					else
 					{
-						cout << "Unsorted: " << ds.comparisonCount() << endl;
+						cout << "Value: " << numEntry << " FOUND     \t\t   Comparisons \t Total" << endl;
+						cout << "Unsorted Array(Linear Search) \t\t" << ds.comparisonCount() << "\t  " << ds.get_linear_total() << endl;
+						inList.push_back(numEntry);
+						inCounter--;
 					}
 				}
-				else
+				else 
 				{
 					if (outCounter == 0)
 					{
-						cout << "5 number entries that match numbers in list already exist." << endl;
+						cout << "You already entered 5 numbers NOT matching file data." << endl;
 						outFull = true;
-						system("PAUSE");
-						ds.comparisonReset();
+						system("PAUSE"); system("CLS");
 						break;
 					}
 					else
 					{
-						cout << "Unsorted: NOT FOUND" << endl;
+						cout << "Value: " << numEntry << " NOT FOUND \t\t   Comparisons \t Total" << endl;
+						cout << "Unsorted Array(Linear Search) \t\t" << ds.comparisonCount() << "\t  " << ds.get_linear_total() << endl;
+						outList.push_back(numEntry);
+						outCounter--;
 					}
 				}
-				ds.comparisonReset();
+				ds.comparisonReset();	// resets comparison count to 0
 
-				if (ds.searchTree_Search(root, numEntry))
-				{
-					cout << "BinSearchTree: " << ds.comparisonCount() << endl;
-					inCounter--;
-				}
-				else
-				{
-					cout << "BinSearchTree: NOT FOUND" << endl;
-					outCounter--;
-				}
-				ds.comparisonReset();
+				// Prints search tree comparison count for current entry and total comparisons for search tree using binary search
+				ds.searchTree_Search(root, numEntry);
+				cout << "Binary Tree(Binary Search) \t\t" << ds.comparisonCount() << "\t  " << ds.get_BST_total() << endl;
+				ds.comparisonReset();	// resets comparison count to 0
 
-				if (ds.sorted_binarySearch(numEntry))
-				{
-					cout << "Sorted: " << ds.comparisonCount() << endl;
-					inList.push_back(numEntry);
-				}
-				else
-				{
-					cout << "Sorted: NOT FOUND" << endl;
-					outList.push_back(numEntry);
-				}
-				ds.comparisonReset();
+				// Prints sorted array comaprison count for current entry and total comparisons for sorted array using binary search
+				ds.sorted_binarySearch(numEntry);
+				cout << "Sorted Array(Binary Search) \t\t" << ds.comparisonCount() << "\t  " << ds.get_BSA_total() << endl;
+				ds.comparisonReset();	// resets comparison count to 0
 
-				system("PAUSE");
+				// Prints lists of user's prior entries
+				cout << "\nCurrent Entries" << endl;
+				cout << "FOUND list   \t";
+				for (unsigned int i = 0; i < inList.size(); i++)
+				{
+					cout << inList[i] << " ";
+				}
+				cout << endl;
+
+				cout << "NOT FOUND list\t";
+				for (unsigned int i = 0; i < outList.size(); i++)
+				{
+					cout << outList[i] << " ";
+				}
+				cout << endl;
+
+				system("PAUSE"); system("CLS");
 				break;
-			}
-			system("CLS");
-			cout << "Entry Complete" << endl;
+			} 
 		}
 	}
+	system("CLS");
+	cout << "Results" << endl;
+	cout << "Unsorted Array " << ds.get_linear_total() << " LINEAR search comparisons" << endl;
+	cout << "Search Tree    " << ds.get_BST_total() << " BINARY search comparisons" << endl;
+	cout << "Sorted Array   " << ds.get_BSA_total() << " BINARY search comparisons" << endl;
+	
+	return 0;
 }
